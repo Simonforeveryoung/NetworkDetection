0ea2e9404bf60597595486cefa8dd54b78742f15
simonforeveryoung <simonforeveryoung@gmail.com>
2012-05-04 10:59:59 -0700

    Initial Commit

---
+++ NetworkDetection/NetworkDetection.h
@@ -0,0 +1,13 @@
+//
+//  NetworkDetection.h
+//  NetworkDetection
+//
+//  Created by SIMON YOUNG on 5/4/12.
+//  Copyright (c) 2012 Jumping Loster. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkDetection : NSObject
+
+@end
---
+++ NetworkDetection/NetworkDetection.m
@@ -0,0 +1,13 @@
+//
+//  NetworkDetection.m
+//  NetworkDetection
+//
+//  Created by SIMON YOUNG on 5/4/12.
+//  Copyright (c) 2012 Jumping Loster. All rights reserved.
+//
+
+#import "NetworkDetection.h"
+
+@implementation NetworkDetection
+
+@end
